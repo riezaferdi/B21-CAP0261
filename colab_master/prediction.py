b1020c51c28008c9930de5e529f534acf3d120f4
WidadAbida <widad7501.war@gmail.com>
2021-05-25 10:17:19 +0000

    add an example

--- colab_master/prediction.py
+++ colab_master/prediction.py
@@ -1,3 +1,5 @@
+from tensorflow.keras.models import Sequential, save_model, load_model
+
 # File path
 filepath ='./saved_model/1'
 
@@ -8,13 +10,30 @@ model = load_model(filepath, compile = True)
 def preprocess(image_name):
     pass
 
-# A few random samples
-#use_samples = [5, 38, 3939, 27389]#change please
-#samples_to_predict = []
+def predictor():
+    pass
+
+"""# A few random samples
+use_samples = [5, 38, 3939, 27389]
+samples_to_predict = []
+
+# Generate plots for samples
+for sample in use_samples:
+  # Generate a plot
+  reshaped_image = input_train[sample].reshape((img_width, img_height))
+  plt.imshow(reshaped_image)
+  plt.show()
+  # Add sample to array for prediction
+  samples_to_predict.append(input_train[sample])
 
 # Convert into Numpy array
-#samples_to_predict = np.array(samples_to_predict)
+samples_to_predict = np.array(samples_to_predict)
+print(samples_to_predict.shape)
 
-# Generate pred.ictions for samples
-#predictions = model.predict(samples_to_predict)
-#print(predictions)
+# Generate predictions for samples
+predictions = model.predict(samples_to_predict)
+print(predictions)
+
+# Generate arg maxes for predictions
+classes = np.argmax(predictions, axis = 1)
+print(classes)"""
