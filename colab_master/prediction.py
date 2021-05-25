8e1c0cc272b6d9716507b8b6244b4123a9eb2c1a
WidadAbida <widad7501.war@gmail.com>
2021-05-25 10:01:16 +0000

    load model, preprocess image and predict

---
+++ colab_master/prediction.py
@@ -0,0 +1,20 @@
+# File path
+filepath ='./saved_model/1'
+
+# Load the model
+model = load_model(filepath, compile = True)
+
+#Preprocess image
+def preprocess(image_name):
+    pass
+
+# A few random samples
+#use_samples = [5, 38, 3939, 27389]#change please
+#samples_to_predict = []
+
+# Convert into Numpy array
+#samples_to_predict = np.array(samples_to_predict)
+
+# Generate pred.ictions for samples
+#predictions = model.predict(samples_to_predict)
+#print(predictions)
